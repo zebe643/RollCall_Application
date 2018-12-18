bee31b1ff43b97066d4ecb4727c712e6a26ea8af
rc-admin-at-796748717407 <burnz@amazon.com>
2018-12-18 16:12:28 +0000

    added code for Lambda function that calls Pinpoint and also redirect page if form is sucessfully submitted

---
+++ index.js
@@ -0,0 +1,66 @@
+
+exports.handler = function(event, context, callback) {
+console.log(event);
+var AWS = require('aws-sdk');
+AWS.config.update({region: 'eu-west-1'});
+var pinpointemail = new AWS.PinpointEmail({apiVersion: '2018-07-26'});
+
+function requestLeave(alias, RequestType, MyShift, StartDate, Duration, RequestReason){ 
+    var confirmationEmail = {
+        Content: {
+            Simple: {
+                Body: {
+                    Text: {
+                        Data: "Hi," + "\n\n" + "I'm requesting some time off due to me being " + RequestType + ".\n" + "My shift for the week is " + MyShift + ".\n" +
+                        "The first date I need off is " + StartDate + " for a total of " + Duration + " days" + ".\n\n" + "Additional Information: " + RequestReason.replace(/%20/g, " ") +
+                        "\n\n" + "Many thanks," + "\n" + alias,
+                        Charset: "UTF-8"
+                    }
+                },
+                  Subject: {
+                  Data: "Rollcall:[" + RequestType + "][" + alias + "][" + StartDate +"]" ,
+                        Charset: "UTF-8"           
+            }
+        }
+    },
+        Destination: {
+            CcAddresses: [alias + "@amazon.com"],
+            ToAddresses: 
+                [alias + "@amazon.com"] //will be set as aws-ps-dms-dub-vacation in prod, sent to burnz@amazon.com in dev/beta
+        },
+        FromEmailAddress: "aws-ps-rollcall@amazon.com"
+    };
+
+    pinpointemail.sendEmail(confirmationEmail, function(err, data) {
+        if (err) console.log(err, err.stack); //error occured
+        else console.log(data); //successful response
+    });
+}
+
+for(var key in event){
+    var user = event["user"]
+    var rType = event["requestType"]
+    var shift = event["shift"]
+    var sDate = event["startdate"]
+    var dur = event["Duration"]
+    var rReason = event["RequestReason"]   
+}
+
+
+requestLeave(user, rType, shift, sDate, dur, rReason);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
